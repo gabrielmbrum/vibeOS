8df627994d94e6d4c8df738438e54ebb3c3d8e99
Brum <gabrielbrum2805@gmail.com>
2025-04-23 16:33:48 -0300

    fix: removing build dir

--- build/CMakeFiles/vibeOS.dir/src/main.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/vibeOS.dir/src/main.c.o: /home/brum/os-proj/src/main.c \
- /usr/include/stdc-predef.h /home/brum/os-proj/src/../include/process.h \
- /home/brum/os-proj/src/../include/debug.h \
- /home/brum/os-proj/src/../include/process.h
