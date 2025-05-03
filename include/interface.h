68094467f90adb1ee148d83741b271e5dfee61ab
MTomain <mtomain3@gmail.com>
2025-05-03 17:49:45 -0300

    update and optimization. Fixed bugs. Latest interface integrated with cmake

--- include/interface.h
+++ include/interface.h
@@ -2,25 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 #include <curses.h>
-
-#define MIN_WD_HGH 1
-#define MAX_WD_HGH_PROCESS 8
 #define MAX_INPUT_STR 25
 #define MAX_OUTPUT_STR 100
+#define NUMBER_OF_WINDOWS 5
 #define POS_X 1
+#define POS_Y 1
+#define DEF_WIN_WDH 59
+#define DEF_WIN_HGH_SMALL 5
+#define DEF_WIN_HGH_MEDIUM 9
+#define DEF_WIN_HGH_BIG 10
+#define DEF_WIN_HGH_BIGGER 16
+#define DEF_WIN_MAX_PRINTS_SMALL 3
+#define DEF_WIN_MAX_PRINTS_BIG 8
+#define DEF_WIN_MAX_PRINTS_BIGGER 14
+#define SYNT 15
 
-WINDOW *create_newwin(int height, int width, int starty, int startx, char* title);
+//! UPDATE THE functions 
 
-//WINDOW *init_windows(WINDOW *janela_menu, WINDOW *janela_output, WINDOW *janela_I_O, WINDOW *janela_memory, WINDOW *janela_process, WINDOW *janela_scheduler);
+// support functions
+void clear_space(int y, int x,int size);
+
+char *generate_blank_space(int size);
+
+// window operations
+WINDOW *create_newwin(int height, int width, int starty, int startx, char *title);
 
 WINDOW *delete_window(WINDOW *local_win);
 
 WINDOW *menu(WINDOW *local_win);
 
 WINDOW *janela_intro();
-
-void clear_input_space(int size, int start, int end);
-
-void print_input(WINDOW *local_win, char **list, int *size);
-
-void add_input_list(char **list, char *input, int *num);
