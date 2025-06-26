c4e501b53b60c9d483ea162beeb2ed3d9d07cbc6
MTomain <mtomain3@gmail.com>
2025-06-26 13:11:18 -0300

    changed color function. Removed warnings

---
+++ build/CMakeFiles/vibeOS.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vibeOS.dir/src/debug.c.o"
+  "CMakeFiles/vibeOS.dir/src/debug.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/instruction.c.o"
+  "CMakeFiles/vibeOS.dir/src/instruction.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/interface.c.o"
+  "CMakeFiles/vibeOS.dir/src/interface.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/iohandler.c.o"
+  "CMakeFiles/vibeOS.dir/src/iohandler.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/kernel.c.o"
+  "CMakeFiles/vibeOS.dir/src/kernel.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/main.c.o"
+  "CMakeFiles/vibeOS.dir/src/main.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/memory.c.o"
+  "CMakeFiles/vibeOS.dir/src/memory.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/process.c.o"
+  "CMakeFiles/vibeOS.dir/src/process.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/program.c.o"
+  "CMakeFiles/vibeOS.dir/src/program.c.o.d"
+  "CMakeFiles/vibeOS.dir/src/semaphore.c.o"
+  "CMakeFiles/vibeOS.dir/src/semaphore.c.o.d"
+  "vibeOS"
+  "vibeOS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vibeOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
